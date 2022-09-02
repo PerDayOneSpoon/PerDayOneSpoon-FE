54f80e6ce66e74f3787d9e513dffd7e8670cf59f
soyeon102 <csea9000@naver.com>
2022-09-02 23:30:39 +0900

    [Add]: layout, router 추가

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+import Layout from '../layout/Layout';
+
+const LoginPage = () => {
+  return <Layout>LoginPage</Layout>;
+};
+
+export default LoginPage;
---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,7 @@
+import Layout from '../layout/Layout';
+
+const MyPage = () => {
+  return <Layout>MyPage</Layout>;
+};
+
+export default MyPage;
