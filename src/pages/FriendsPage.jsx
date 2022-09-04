5462eb6c3e150d7aad78af4748fbdef1fa4d9c80
baejii <ttsbu@naver.com>
2022-09-05 01:54:50 +0900

    [Add] 비로그인시 로그인페이지 외 접근 차단

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,6 +1,18 @@
 import Layout from '../layout/Layout';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const FriendsPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access-token');
+
+    if (accessToken == null || accessToken === '') {
+      navigate('/login');
+    }
+  }, []);
+
   return <Layout>FriendsPage</Layout>;
 };
 
