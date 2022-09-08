3154b4ff745f6cc0dc09afee9a281d30779ff431
baejii <ttsbu@naver.com>
2022-09-08 14:05:35 +0900

    [Feat] 마이페이지 내 정보 수정 구현중

--- src/components/user/UserInfo.jsx
+++ src/components/user/UserInfo.jsx
@@ -2,8 +2,19 @@ import styled from 'styled-components';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import { userApi } from '../../api/userApi';
 import { colors } from '../../theme/theme';
+import { useState, useRef } from 'react';
 
 const UserInfo = ({ isMypage }) => {
+  const queryClient = useQueryClient();
+
+  const [onlyView, setOnlyView] = useState(true);
+  const profileImg = useRef();
+
+  const [post, setPost] = useState({
+    nickname: '',
+    status: '',
+  });
+
   const {
     isLoading,
     isError,
@@ -11,9 +22,52 @@ const UserInfo = ({ isMypage }) => {
     data: userInfo,
   } = useQuery('getUserInfo', userApi.getUserInfo, {
     onSuccess: (data) => {
-      console.log('user정보', data);
+      console.log('user정보!!!!!!!!!!', data);
+      setPost({
+        nickname: data.nickname,
+        status: data.status,
+      });
     },
   });
+
+  const updateUserStatusMutation = useMutation(userApi.updateUserStatus, {
+    onSuccess: () => {
+      setOnlyView(!onlyView);
+    },
+  });
+
+  // const temp = (e) => {
+  //   console.log('change text...');
+  //   console.log(e);
+  // };
+
+  const onChangeHandler = (e) => {
+    const { value, name } = e.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+    console.log(post);
+    console.log(e.target.value);
+  };
+
+  const doModify = (e) => {
+    console.log(onlyView);
+
+    if (onlyView) {
+      setOnlyView(!onlyView);
+    } else {
+      updateUserStatusMutation.mutate(post);
+    }
+  };
+
+  const onCickImageUpload = () => {
+    profileImg.current.click();
+  };
+
+  const updateProfile = (e) => {
+    console.log(e.target.files);
+  };
 
   if (isLoading) {
     return <div>로딩 중..</div>;
@@ -26,22 +80,46 @@ const UserInfo = ({ isMypage }) => {
           <ProfileImgContainer>
             <ProfileImg src={userInfo.data.profileImage} />
           </ProfileImgContainer>
-          <ChangingText>프로필사진바꾸기</ChangingText>
-          <Status>{userInfo.data.status}</Status>
+          <input
+            type='file'
+            ref={profileImg}
+            onChange={updateProfile}
+            style={{ display: 'none' }}
+          />
+          <ChangingText onClick={onCickImageUpload}>
+            프로필사진바꾸기
+          </ChangingText>
+          <Statue>{userInfo.data.status}</Statue>
         </Top>
         <Middle>
           <SettingForm>
             <FormLeft>이름</FormLeft>
-            <FormRight>{userInfo.data.nickname}</FormRight>
+            <input
+              type='text'
+              defaultValue={userInfo.data.nickname}
+              name='nickname'
+              onChange={onChangeHandler}
+              // onChange={temp('nickname')}
+              disabled={onlyView}
+            />
           </SettingForm>
           <SettingForm>
             <FormLeft>상태 메세지</FormLeft>
-            <FormRight>{userInfo.data.status}</FormRight>
+            <input
+              type='text'
+              defaultValue={userInfo.data.status}
+              name='status'
+              onChange={onChangeHandler}
+              // onChange={temp('status')}
+              disabled={onlyView}
+            />
+            {/* <FormRight>{userInfo.data.status}</FormRight> */}
           </SettingForm>
           <SettingForm>
             <FormLeft>검색 코드</FormLeft>
             <FormRight>{userInfo.data.socialCode}</FormRight>
           </SettingForm>
+          <button onClick={doModify}>{onlyView ? '수정' : '저장'}</button>
         </Middle>
       </Container>
     );
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import LoginPage from '../pages/LoginPage';
 import CollectionPage from '../pages/CollectionPage';
@@ -8,7 +6,6 @@ import CreatePage from '../pages/CreatePage';
 import Calendar from '../pages/CalendarPage';
 import MyPage from '../pages/MyPage';
 import SearchPage from '../pages/SearchPage';
-import SettingPage from '../pages/SettingPage';
 import KakaoLogin from '../components/login/KakaoLogin';
 import GoogleLogin from '../components/login/GoogleLogin';
 
@@ -25,7 +22,6 @@ const Router = () => {
         <Route path='/create' element={<CreatePage />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='/search' element={<SearchPage />} />
-        <Route path='/setting' element={<SettingPage />} />
       </Routes>
     </BrowserRouter>
   );
