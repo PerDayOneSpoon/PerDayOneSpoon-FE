6291e864e230c9984c65100725e4a2f0712188e4
baejii <ttsbu@naver.com>
2022-09-10 02:42:04 +0900

    [Style] 검색페이지 CSS 작업중

--- src/components/friends/FriendsItem.jsx
+++ src/components/friends/FriendsItem.jsx
@@ -1,16 +1,34 @@
+import styled from 'styled-components';
+import { colors } from '../../theme/theme';
+
 const FriendsItem = ({ val }) => {
   const followhandler = () => {
     console.log(val.socialid);
   };
 
   return (
-    <>
-      <div>
-        <p>{val.userId}</p>
-        <button onClick={followhandler}>test</button>
-      </div>
-    </>
+    <SearchList>
+      <p>{val.userId}</p>
+      <FollowButton onClick={followhandler}>팔로우</FollowButton>
+    </SearchList>
   );
 };
 
 export default FriendsItem;
+
+const SearchList = styled.div`
+  width: 100%;
+  height: 56px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FollowButton = styled.button`
+  width: 68px;
+  height: 28px;
+  background-color: ${colors.primary};
+  border: none;
+  border-radius: 14px;
+  color: white;
+`;
