5c816b5efb1d7a603eb5d08272ea978673c2c428
soyeon102 <csea9000@naver.com>
2022-10-16 15:07:46 +0900

    [Etc] react-query devtool 주석 해제

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
     <RecoilRoot>
-      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
+      <ReactQueryDevtools initialIsOpen={true} />
       <App />
     </RecoilRoot>
   </QueryClientProvider>
